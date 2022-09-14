0a2056bbfccec31c3bd08d48a750dd8503e83cd3
Hoosang Lee <lhs223@gmail.com>
2022-09-14 10:31:00 +0900

    Update launch_docker.sh
    update working directory

--- launch_docker.sh
+++ launch_docker.sh
@@ -2,6 +2,7 @@ sudo docker run --gpus all -it --net=host --privileged \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -e DISPLAY=unix$DISPLAY \
     -v `pwd`/share:/root/share \
+    -w /root/share \
     --name unity \
     lhs223/hsa_project:unity \
     bash
